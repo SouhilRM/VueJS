756352befbd840531a72d3c21baba605420e83f6
SouhilRM <r.m.souhil47@gmail.com>
2023-03-05 21:09:18 +0100

    introduction au CLI partie 01

---
+++ 05.CLI/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <h2>Ma liste d'amis : </h2>
+        <ul>
+            <li>ami 01</li>
+            <li>ami 02</li>
+        </ul>
+    </section>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                amis: [
+                    {
+                        id: '01',
+                        nom: 'Ramdane Mamcha',
+                        prenom: 'Souhil',
+                        age: 23,
+                        tel: '0669884477'
+                    },
+                    {
+                        id: '02',
+                        nom: 'Bekara',
+                        prenom: 'Ghizlen',
+                        age: 30 ,
+                        tel: '0699999999',
+                    }
+                ]
+            }
+        }
+    }
+</script>
